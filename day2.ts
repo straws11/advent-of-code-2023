7fc659a82f23ca2472d96356cd55341d4d8c5fe1
straws-laptop <27012115@sun.ac.za>
2023-12-02 18:28:45 +0200

    Final day 2 solution

--- day2.ts
+++ day2.ts
@@ -45,9 +45,51 @@ function both(part1: boolean): number {
     if (validGame) total += idx + 1;
     part2Total += [...max].reduce((acc, cur) => acc * cur, 1);
   });
-  if (part1) return total;
-  else return part2Total;
+  return part1 ? total : part2Total;
 }
+
+
+function newBoth(part1: boolean = false): number {
+
+  var total: number = 0;
+  var part2Total: number = 0;
+
+  contents.forEach((val, idx) => {
+    var validGame: boolean = true;
+    var max: number[] = [0, 0, 0];
+    const balls: string[] = val.slice(val.indexOf(':') + 2, val.length).split('; ').join(', ').split(', ');
+    for (const ball of balls) {
+      const [am, color] = ball.split(' ');
+      const amount = Number(am);
+      switch (color) {
+        case ('red'): {
+          max[0] = Math.max(amount, max[0]);
+          validGame = amount <= 12;
+          break;
+        }
+        case ('green'): {
+          max[1] = Math.max(amount, max[1]);
+          validGame = amount <= 13;
+          break;
+        }
+        case ('blue'): {
+          max[2] = Math.max(amount, max[2]);
+          validGame = amount <= 14;
+        }
+      }
+
+      if (!validGame && part1) break;
+
+    }
+
+    if (validGame) total += idx + 1;
+    part2Total += [...max].reduce((acc, cur) => acc * cur, 1);
+  });
+
+  return part1 ? total : part2Total;
+}
+
 
 console.log(`Part 1: ${both(true)}`);
 console.log(`Part 2: ${both(false)}`);
+console.log(`Chadder Solutions: ${newBoth(true)}, ${newBoth(false)}`);
